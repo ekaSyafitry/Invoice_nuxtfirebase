3c46881071a998bc9637aa56b0782da59e2690d2
ekaSyafitry <ekasyafitry93@gmail.com>
2021-08-11 18:45:38 +0700

    update index

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -79,42 +79,6 @@ import modal from '~/components/modal.vue'
             console.log(val, 'dff')
             this.showModal = val
         },
-      makeid(length) {
-        var result = '';
-        var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        var charactersLength = characters.length;
-        for (var i = 0; i < length; i++) {
-          result += characters.charAt(Math.floor(Math.random() * charactersLength));
-        }
-        return result;
-      },
-      async removeData(key) {
-        if (!confirm('are you sure delete this invoices')) {
-          return
-        }
-        try {
-          let invoices = this.$fire.database.ref('invoices/' + key);
-          invoices.remove()
-        } catch (error) {
-          alert(e)
-          return
-        }
-        alert('Success, Remove Data')
-        this.readFromRealtimeDb()
-      },
-      async updateData(key) {
-        try {
-          let invoices = this.$fire.database.ref('invoices/' + key);
-          invoices.update({
-            amount_invoice: 5000
-          })
-        } catch (error) {
-          alert(e)
-          return
-        }
-        alert('Success, Update Data')
-        this.readFromRealtimeDb()
-      },
       async readFromRealtimeDb(status = "") {
         const dbref = this.$fire.database.ref('invoices')
         try {
@@ -134,50 +98,6 @@ import modal from '~/components/modal.vue'
         console.log('this.invoices', this.invoices)
         console.log('this.invoice_data', this.invoice_data)
       },
-      async writeToRealtimeDb() {
-        const messageRef = this.$fire.database.ref('invoices')
-        try {
-          await messageRef.push().set({
-            invoice_id: this.makeid(5),
-            status: 'Paid',
-            bill_from: {
-              street: 'Pondok Ungu Permai',
-              city: 'bekasi',
-              postcode: '17125',
-              country: 'Indonesia'
-            },
-            bill_to: {
-              name: 'Eka Syafitry Dewi',
-              email: 'eka@gmail.com',
-              street: 'Pondok Ungu Permai',
-              city: 'bekasi',
-              postcode: '17125',
-              country: 'Indonesia'
-            },
-            invoice_date: new Date('2021-08-10').getTime(),
-            payment_term: 60,
-            project_description: 'Graphic Design',
-            item_list: [{
-                item_name: 'Banner Design',
-                qty: 2,
-                price: 10000,
-                total: 20000
-              },
-              {
-                item_name: 'Email Design',
-                qty: 4,
-                price: 15000,
-                total: 60000
-              },
-            ],
-            amount_invoice: 100000, // total of item
-          })
-        } catch (e) {
-          alert(e)
-          return
-        }
-        alert('Success. Create Invoice')
-      }
     }
   }
 
